eaec2e2c70740f778639c14d864da3c8307a6cfd
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-02-16 14:23:49 -0300

    deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist

--- backend/src/test/java/com/fmrm/catalog/services/ProductServiceTestsIT.java
+++ backend/src/test/java/com/fmrm/catalog/services/ProductServiceTestsIT.java
@@ -1,14 +1,22 @@
 package com.fmrm.catalog.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fmrm.catalog.repositories.ProductRepository;
+import com.fmrm.catalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
 public class ProductServiceTestsIT {
 	
 	@Autowired
 	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
 	
 	private Long existingId;
 	private Long nonExistingId;
@@ -22,7 +30,18 @@ public class ProductServiceTestsIT {
 	}
 	
 	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);			
+		});
+	}
+	
+	@Test
 	public void deleteShouldDeleteResourceWhenIdExists() {
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
 		
 	}
 }

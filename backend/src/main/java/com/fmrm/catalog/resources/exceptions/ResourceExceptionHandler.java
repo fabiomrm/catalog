5993f4b9094729c3188fe8a068e6c772fce8a30f
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-02-19 20:44:31 -0300

    Custom validation response

--- backend/src/main/java/com/fmrm/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/fmrm/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -41,19 +42,24 @@ public class ResourceExceptionHandler {
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
+		
 		return ResponseEntity.status(status.value()).body(error);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request ) {
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request ) {
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		
-		StandardError error = new StandardError();
+		ValidationError error = new ValidationError();
 		error.setTimestamp(Instant.now());
 		error.setStatus(status.value());
 		error.setError("Validation exception");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
+		
+		for (FieldError fe : e.getBindingResult().getFieldErrors()) {
+			error.addError(fe.getField(), fe.getDefaultMessage());
+		}
 		
 		return ResponseEntity.status(status.value()).body(error);
 	}

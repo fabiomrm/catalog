c0fe3f5bc883d0ea34a95b932cb2b44ea703899a
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-02-20 19:03:21 -0300

    UserUpdateValidator

--- backend/src/main/java/com/fmrm/catalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/fmrm/catalog/services/validation/UserUpdateValidator.java
@@ -1,19 +1,24 @@
 package com.fmrm.catalog.services.validation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
-import com.fmrm.catalog.dto.UserInsertDTO;
+import com.fmrm.catalog.dto.UserUpdateDTO;
 import com.fmrm.catalog.entities.User;
 import com.fmrm.catalog.repositories.UserRepository;
 import com.fmrm.catalog.resources.exceptions.FieldMessage;
 
-public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserInsertDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
 	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository repository;
@@ -23,12 +28,16 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 	}
 
 	@Override
-	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uriVars.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		User user = repository.findByEmail(dto.getEmail());
-		if(user != null) {
+		if(user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "Emal já cadastrado"));
 		}
 		

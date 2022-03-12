d12db7c9281caaac43dd1f2047ad0ec552da0b62
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-12 20:53:20 -0300

    prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@ import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
-  return (
-    <h1 className="bg-primary">Olá Catalog!</h1>
-  );
+  return <h1>Olá Catalog!</h1>;
 }
 
 export default App;

45be65eb68fd4bdcc16109da0f5e484e8dff4be0
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-13 03:41:22 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import { Navbar } from 'components/Navbar';
 
 function App() {
-  return <h1>Olá Catalog!</h1>;
+  return (
+    <Navbar />
+  );
 }
 
 export default App;

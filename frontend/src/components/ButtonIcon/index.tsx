a62abea87e64031073fae30e8d4e54dcd04ff51a
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-13 04:32:21 -0300

    Responsive small

---
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css';
+
+export const ButtonIcon = () => {
+  return (
+    <button className="btn btn-primary btn-icon">
+        <span>INICIE AGORA A SUA BUSCA</span>
+    </button>)
+};
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,24 @@
+import { Navbar } from "components/Navbar"
+import { ButtonIcon } from "components/ButtonIcon";
+import { ReactComponent as MainImage } from "assets/images/main-img.svg";
+import './styles.css';
+
+export const Home = () => {
+    return(
+        <>
+            <Navbar />
+            <div className="home-container">
+                <div className="home-card">
+                    <div className="home-content-container">
+                        <h1>Conheça o melhor catálogo de produtos</h1>
+                        <p>Ajudaremos você a encontrar os melhores produtos disponíveis.</p>
+                        <ButtonIcon />
+                    </div>
+                    <div className="home-image-container">
+                        <MainImage />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

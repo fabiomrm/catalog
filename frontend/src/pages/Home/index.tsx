7a4b83b2d2352fb1e1d9a4813abc56f10575de97
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-13 07:17:08 -0300

    finish Home responsive layout

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,8 +10,10 @@ export const Home = () => {
             <div className="home-container">
                 <div className="home-card">
                     <div className="home-content-container">
-                        <h1>Conheça o melhor catálogo de produtos</h1>
-                        <p>Ajudaremos você a encontrar os melhores produtos disponíveis.</p>
+                        <div>
+                            <h1>Conheça o melhor catálogo de produtos</h1>
+                            <p>Ajudaremos você a encontrar os melhores produtos disponíveis.</p>
+                        </div>
                         <ButtonIcon />
                     </div>
                     <div className="home-image-container">

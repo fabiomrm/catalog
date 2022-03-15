0e262a71fc232581cc70ce59a7f29e20cd337b6b
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-15 19:19:58 -0300

    React hook form

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -1,23 +1,37 @@
 import { Link } from 'react-router-dom';
-import {ButtonIcon} from 'components/ButtonIcon';
+import { ButtonIcon } from 'components/ButtonIcon';
+import { useForm } from 'react-hook-form';
 
 import './styles.css';
 
+type FormData = {
+  username: string;
+  password: string;
+}
+
 export const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    
+  };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            {...register('username')}
+            name="username"
             type="text"
             className="form-control base-input"
             placeholder="Email"
-            name="username"
           />
         </div>
         <div className="mb-2">
           <input
+            {...register('password')}
             type="password"
             className="form-control base-input "
             placeholder="Password"

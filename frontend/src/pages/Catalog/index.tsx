ab4f235812e9397911aaf48fb567696e4f183559
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-15 09:59:42 -0300

    CarLoader

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -4,24 +4,34 @@ import { Link } from 'react-router-dom';
 
 import './styles.css';
 import { Pagination } from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'utils/requests';
+import axios from 'axios';
+import { CardLoader } from './CardLoader';
 
 export const Catalog = () => {
-  const product: Product = {
-    id: 3,
-    name: 'Macbook Pro',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 1250.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg',
-    date: '21/06/1996',
-    categories: [
-      {
-        id: 3,
-        name: 'Computadores',
+  const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
       },
-    ],
-  };
+    };
+
+    setIsLoading(() => true);
+    axios(params)
+      .then(({ data }) => {
+        setPage(data);
+      })
+      .finally(() => setIsLoading(() => false));
+  }, []);
 
   return (
     <div className="container my-4 catalog-container">
@@ -29,31 +39,18 @@ export const Catalog = () => {
         <h1>Catálogo de Produtos</h1>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
-          <Link to="/products/1">
-            <ProductCard product={product} />
-          </Link>
-        </div>
+        {isLoading ? (
+          <CardLoader />
+        ) : (
+          page?.content &&
+          page.content.map((product, _) => (
+            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+              <Link to={`/products/${product.id}`}>
+                <ProductCard product={product} />
+              </Link>
+            </div>
+          ))
+        )}
       </div>
       <div className="row">
         <Pagination />

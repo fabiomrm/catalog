be1b18078b9679ba8631b578b54dc615ed915bd0
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-21 07:28:32 -0300

    Product crud form

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,3 +1,31 @@
+import './styles.css';
+
 export const Form = () => {
-  return <h1>Tela Form</h1>;
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-card-form">
+        <h1 className="product-crud-card-form-title">Dados do Produto</h1>
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+              <input type="text" className="form-control base-input" />
+              <input type="text" className="form-control base-input" />
+              <input type="text" className="form-control base-input" />
+            </div>
+            <div className="col-lg-6">
+              <textarea
+                name=""
+                rows={10}
+                className="form-control base-input"
+              ></textarea>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-outline-primary">SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };

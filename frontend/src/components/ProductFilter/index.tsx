f7636637448d1a919915f5ac063a4f41b2aec88f
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-03-23 15:30:09 -0300

    ProductFilter basic HTML

---
+++ frontend/src/components/ProductFilter/index.tsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+export const ProductFilter = () => {
+  return (
+    <div className="base-card product-filter-container">
+      <form className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input type="text" className="form-control" />
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select name="" id="">
+              <option value="">Livros</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">LIMPAR</button>
+        </div>
+      </form>
+    </div>
+  );
+};
